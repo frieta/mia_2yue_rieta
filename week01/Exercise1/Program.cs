1ce1305a83f5d4348b5e48deaaaca1aeabbadad3
frieta <ferdinan.lim.rieta@gmail.com>
2025-03-03 14:47:12 +0800

    Week01-Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+      {
+        Console.Write("What is your first name? ");// ask the user for their first name
+        string firstName = Console.ReadLine();//get the user input for first name
+
+        Console.Write("What is your last name? ");// ask the user for their last name
+        string lastName = Console.ReadLine();//get the user input for last name
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");// Display the output
     }
 }
